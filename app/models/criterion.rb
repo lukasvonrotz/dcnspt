70f88c202fc3c4033e7c6f79fe122231473b27cf
Lukas von Rotz <lukas.v.rotz@hotmail.com>
2017-02-18 14:11:46 +0100

    bugfix when deleting employee or criterion

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -1,7 +1,7 @@
 class Criterion < ActiveRecord::Base
-  has_many :criterionvalues
+  has_many :criterionvalues, dependent: :delete_all
   has_many :employees, through: :criterionvalues
-  has_many :criterionparams
+  has_many :criterionparams, dependent: :delete_all
   has_many :projects, through: :criterionparams
   belongs_to :criterioncontext
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,8 +1,8 @@
 class Employee < ActiveRecord::Base
   has_and_belongs_to_many :projects
-  has_many :criterionvalues, dependent: :destroy
+  has_many :criterionvalues, dependent: :delete_all
   has_many :criterions, through: :criterionvalues
-  has_many :workloads, dependent: :destroy
+  has_many :workloads, dependent: :delete_all
   has_many :weeks, through: :workloads
   belongs_to :jobprofile
 
